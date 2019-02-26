a4c1af46f80be054ef9d46433f36f42ef852cb06
Mateusz <matteuszd@gmail.com>
2019-02-26 11:39:04 +0100

    Add 'add player' form

--- src/routes.js
+++ src/routes.js
@@ -11,11 +11,24 @@ import Dashboard from "./components/admin/Dashboard";
 import AdminMatches from "./components/admin/matches";
 import AddEditMatch from "./components/admin/matches/addEditMatch";
 import AdminPlayers from "./components/admin/players";
+import AddEditPlayers from "./components/admin/players/addEditPlayers";
 
 const Routes = props => {
   return (
     <Layout>
       <Switch>
+        <PrivateRoute
+          {...props}
+          path="/admin_players/add_players"
+          exact
+          component={AddEditPlayers}
+        />
+        <PrivateRoute
+          {...props}
+          path="/admin_players/add_players/:id"
+          exact
+          component={AddEditPlayers}
+        />
         <PrivateRoute
           {...props}
           path="/admin_players"
