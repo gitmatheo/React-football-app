94143f549518aaafb8348f77c2b3e09ef3cda89d
Mateusz <matteuszd@gmail.com>
2019-02-26 08:47:17 +0100

    Add form for 'edit match' section

--- src/routes.js
+++ src/routes.js
@@ -9,11 +9,18 @@ import Home from "./components/home";
 import SignIn from "./components/signIn";
 import Dashboard from "./components/admin/Dashboard";
 import AdminMatches from "./components/admin/matches";
+import AddEditMatch from "./components/admin/matches/addEditMatch";
 
 const Routes = props => {
   return (
     <Layout>
       <Switch>
+        <PrivateRoute
+          {...props}
+          path="/admin_matches/edit_match/:id"
+          exact
+          component={AddEditMatch}
+        />
         <PrivateRoute
           {...props}
           path="/admin_matches"
